d7f90de71b6653af694d8084bc36df2e4e2cd8b9
isnochys <isnochys@gmail.com>
2018-05-12 10:37:18 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -7,11 +7,11 @@ do
        p) pyth=$OPTARG;;
        e) pythscript=$OPTARG;;
        d) direct=$OPTARG;;
-	   ?) echo "Usage ($0) -p python_executable -d source_directory -e pythonscript\n";;
+           ?) echo "Usage ($0) -p python_executable -d source_directory -e pythonscript\n";;
    esac
 done
 logf=$direct/$pythscript.log
-script=$direct/$pythscript 
+script=$direct/$pythscript
 
 ans=`ps -efww | grep -w $script |grep -v grep|wc -l`
 
